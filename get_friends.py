f236fa16a6077a99f96b983571ae980a0691ee06
anirudt <anirudt@gmail.com>
2015-05-18 18:29:49 +0530

    removed info

--- get_friends.py
+++ get_friends.py
@@ -1,10 +1,10 @@
 import facebook
 
 # Makes a graph object connected with this user access token.
-graph = facebook.GraphAPI(access_token = "CAACEdEose0cBANUxhUfL4lOQ9MVfKpCHusD9LGSgKLZCrDUUMZA5DLPMe0W4KTZBxdoZCNoRZCb3tmGAB3YIVpZBuy7ZBeXfx8Sms2sRColiYoBuZCIaYtQumZCIuHisiaEEyhZCx4n65UmA9PobCilmp4ETxK2XtilRuaZCmhGAZC5M312MnvnyCyBtPEeXp0N8Gjno2LJur5FAxV0I3ZBrpIYc8")
+graph = facebook.GraphAPI(access_token = "your_access_token_here")
 
 # Finds the list of 'authenticated' friends of the user having this ID
-friends = graph.get_connections(id="736639463043996", connection_name = "friends")
+friends = graph.get_connections(id="your_profile_id", connection_name = "friends")
 print len(friends["data"])
 
 
